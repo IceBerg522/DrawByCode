6b176710f1cd6326c0f6eff85f0f4e12f25e4542
IceBerg522 <sulitpaul@gmail.com>
2016-05-19 15:03:09 -0500

    code for canvas app

--- app/src/main/java/com/example/mrb/drawbycode/DrawingArea.java
+++ app/src/main/java/com/example/mrb/drawbycode/DrawingArea.java
@@ -2,6 +2,7 @@ package com.example.mrb.drawbycode;
 
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.graphics.BitmapShader;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
@@ -18,7 +19,15 @@ public class DrawingArea extends View // You need to extend the View class
 
     Paint pntTextPaint;     // Painting object to handle the paint jobs for text
     Paint pntBlackPaint;    // Painting object to handle the paint jobs for graphics
-    Paint pntRedPaint;      // Painting object to handle the paint jobs for graphics
+    Paint pntBlackBodPaint;      // Painting object to handle the paint jobs for graphics
+    Paint pntBlckRArmPaint;
+    Paint pntBlckLArmPaint;
+    Paint pntBlckLlegPaint;
+    Paint pntBlckRlegPaint;
+    Paint pntRedBoxPaint;
+    Paint pntBlackBoxPaint;
+    float startX, startY, stopX, stopY;
+    int squareShape = 4000;
 
     public DrawingArea(Context context, AttributeSet attrs)
     {
@@ -46,16 +55,39 @@ public class DrawingArea extends View // You need to extend the View class
 
         pntTextPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         pntBlackPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        pntRedPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        pntBlackBodPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        pntBlckRArmPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        pntBlckLArmPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        pntBlckLlegPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        pntBlckRlegPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        pntRedBoxPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        pntBlackBoxPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
 
         pntBlackPaint.setColor(Color.BLACK);
         pntBlackPaint.setStrokeWidth(3);
 
-        pntRedPaint.setColor(Color.RED);
-        pntRedPaint.setStrokeWidth(3);
+        pntBlackBodPaint.setColor(Color.BLACK);
+        pntBlackBodPaint.setStrokeWidth(3);
+
+        pntBlckRArmPaint.setColor(Color.BLACK);
+        pntBlckRArmPaint.setStrokeWidth(10);
+
+        pntBlckLArmPaint.setColor(Color.BLACK);
+        pntBlckLArmPaint.setStrokeWidth(10);
+
+        pntBlckLArmPaint.setColor(Color.BLACK);
+        pntBlckLArmPaint.setStrokeWidth(10);
+
+        pntBlckRlegPaint.setColor(Color.BLACK);
+        pntBlckRlegPaint.setStrokeWidth(10);
+
+        pntBlackBoxPaint.setColor(Color.BLACK);
+        pntBlackBoxPaint.setStrokeWidth(10);
+
+        pntRedBoxPaint.setColor(Color.RED);
+        pntRedBoxPaint.setStrokeWidth(10);
 
     }
-
     protected void onSizeChanged(int w, int h, int oldw, int oldh)
     {
         // This method exists to handle re-drawing of the DrawingArea object in the event of a
@@ -66,18 +98,42 @@ public class DrawingArea extends View // You need to extend the View class
 
     }
 
-    protected void onDraw(Canvas canvas)
-    {
+    protected void onDraw(Canvas canvas) {
         // This method is where we issue our actual drawing commands.
         // The Canvas parameter is what we draw ON; the Paint objects defined above are what we draw WITH.
 
         super.onDraw(canvas);
 
-        canvas.drawCircle((float) 50.0, (float) 50.0, (float) 50.0, pntBlackPaint);
+        //Code for stickman
+        // canvas.drawCircle((float) 350.0, (float) 50.0, (float) 50.0, pntBlackPaint);
 
-        canvas.drawRect((float) 50.0, (float) 50.0, (float) 350.0, (float) 350.0, pntRedPaint);
+        // canvas.drawRect((float) 355.0, (float) 365.0, (float) 345.0, (float) 100.0, pntBlackBodPaint);
+
+        // canvas.drawLine((float) 500.0, (float)200.0, (float) 350.0, (float) 150.0, pntBlckRArmPaint);
+
+        //  canvas.drawLine((float) 350.0, (float) 150.0, (float) 200.0, (float) 150.0, pntBlckLArmPaint);
+
+        //     canvas.drawLine((float) 500.0, (float) 150.0, (float) 350.0, (float) 200.0, pntBlckRlegPaint);
+
+
+        int width = canvas.getWidth();
+        int height = canvas.getHeight();
+
+        for (int i = 1; i < 50; i++) {
+            startX = width / 2 - squareShape;
+            startY = i * 100;
+            stopX = width / 2 + squareShape;
+            stopY = i * 100;
+            canvas.drawRect(startX, startY, stopX, stopY, pntRedBoxPaint);
+        }
+
+        for (int i = 1; i < 50; i++) {
+            startX = i * 100;
+            startY = height / 2 - squareShape;
+            stopX = i * 100;
+            stopY = height / 2 + squareShape;
+            canvas.drawRect(startX, startY, stopX, stopY, pntBlackBoxPaint);
+        }
 
     }
-
-
 }
